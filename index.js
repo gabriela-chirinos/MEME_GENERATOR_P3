d210af203d108b21c71ffe011bef567df28c985e
Gabriela-Chirinos <chirinos@outlook.com>
2024-09-23 07:19:44 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,131 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+// Creating the Elements: 
+const memeContainer = document.getElementById('container');
+
+
+//header
+const memePlaceholder = document.querySelector('h1');
+
+
+//memo 
+
+const memoPlaceholder = document.querySelector('p');
+
+
+
+//Image
+const memeImage = document.getElementById('meme_image'); 
+
+
+
+//URL
+const urlInput = document.getElementById('image_url');
+
+
+// TOP TEXT
+
+const topTextInput = document.getElementById('top_text');
+
+//BOTTOM TEXT
+
+const bottomTextInput = document.getElementById('bottom_text');
+
+
+// Dynamically creatting P elemets  for text 
+
+const topTextOverlay = document.createElement('p');
+topTextOverlay.classList.add('topTextOverlay'); 
+
+
+
+const bottomTextOverlay = document.createElement('p');
+bottomTextOverlay.classList.add('bottomTextOverlay');
+
+
+//Image container
+
+const imageContainer = document.getElementById('imageContainer');
+
+
+
+
+//append container
+
+
+memeContainer.appendChild(memePlaceholder);
+memeContainer.appendChild(memoPlaceholder);
+imageContainer.appendChild(memeImage);
+
+ // Append the text elements to the image container
+
+
+
+// Local Storage - Not clearing
+
+let storedURL = sessionStorage.getItem('image_url');
+
+if (storedURL) {
+  memeImage.src = storedURL;
+  imageContainer.appendChild(memeImage); // Append the image if it was stored
+}
+
+
+//Click event
+const generateButton = document.getElementById('generate_button');
+generateButton.addEventListener('click',generateMeme);
+
+
+// Generate meme
+
+function generateMeme() {
+
+
+      let imageURL = urlInput.value;
+    
+
+      memeImage.src = imageURL;
+      
+       // Store the image URL in session storage
+      sessionStorage.setItem('image_url', imageURL); 
+
+      //append image to container
+      imageContainer.appendChild(memeImage);
+
+    
+
+      memeImage.onload = () => {
+
+        const topText = topTextInput.value;
+        const bottomText = bottomTextInput.value;
+
+        topTextOverlay.textContent = topText;
+        bottomTextOverlay.textContent = bottomText;
+
+
+        memeContainer.appendChild(topTextOverlay);
+        memeContainer.appendChild(bottomTextOverlay);
+
+
+      }
+
+  
+
+  memeImage.onerror = () => {
+      alert('Failed to load image. Please check the URL. ');
+
+
+      // adding text to image
+
+    };
+  };
+
+})
+
+
+
+
+
+
+
